d6fc590c124c41568fffa8d50fa08a27a4653b5c
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-02-24 21:08:21 +0900

    クエリパラメータからランダムで１つ街名を取得するまで

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express')
 const app = express()
 
+const { getCity } = require('../modules/address');
+
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 
@@ -9,3 +11,8 @@ const server = http.createServer(app);
 server.listen(app.get('port'), () => {
 	console.log(`Server listening on port ${app.get('port')}...`)
 })
+
+app.get('/seach', function (req, res) {
+  let city = getCity(req.query.address);
+  const.log(city);
+});
